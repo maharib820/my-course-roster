84da42cf3ef3f0ea6c02bce8f159be484cd1bcb8
maharib820 <maharib820@gmail.com>
2023-09-16 13:38:28 +0600

    Header part complited

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+    return (
+        <div className="pt-12 pb-8">
+            <header>
+                <h1 className="text-3xl font-bold text-center">Course Registration</h1>
+            </header>
+        </div>
+    );
+};
+
+export default Header;
